3f855ee768331dc5b928a1de214bda4461786dd0
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-06 00:42:09 +0530

    Added DAO for Unit

--- app/src/main/java/com/example/weatherforecast/data/WeatherDao.kt
+++ app/src/main/java/com/example/weatherforecast/data/WeatherDao.kt
@@ -2,6 +2,7 @@ package com.example.weatherforecast.data
 
 import androidx.room.*
 import com.example.weatherforecast.model.Favourite
+import com.example.weatherforecast.model.Unit
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -23,4 +24,20 @@ interface WeatherDao {
 
     @Delete
     suspend fun deleteFavourite(favourite: Favourite)
+
+//    Unit table
+    @Query("SELECT * from settings_table")
+    fun getUnits(): Flow<List<Unit>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUnit(unit: Unit)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateUnit(unit: Unit)
+
+    @Query("DELETE from settings_table")
+    suspend fun deleteAllUnits()
+
+    @Delete
+    suspend fun deleteUnit(unit: Unit)
 }
