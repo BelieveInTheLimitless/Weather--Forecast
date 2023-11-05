992b0ff3ba5ba05e3ee69dfecc045da86c6319be
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-06 00:45:50 +0530

    Added Unit access functions to DB Repository

--- app/src/main/java/com/example/weatherforecast/repository/WeatherDBRepository.kt
+++ app/src/main/java/com/example/weatherforecast/repository/WeatherDBRepository.kt
@@ -2,6 +2,7 @@ package com.example.weatherforecast.repository
 
 import com.example.weatherforecast.data.WeatherDao
 import com.example.weatherforecast.model.Favourite
+import com.example.weatherforecast.model.Unit
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -18,5 +19,16 @@ class WeatherDBRepository @Inject constructor(private val weatherDao: WeatherDao
     suspend fun deleteFavourite(favourite: Favourite) = weatherDao.deleteFavourite(favourite)
 
     suspend fun getFavById(city: String): Favourite = weatherDao.getFavById(city)
+
+//    Unit
+    fun getUnits(): Flow<List<Unit>> = weatherDao.getUnits()
+
+    suspend fun insertUnit(unit: Unit) = weatherDao.insertUnit(unit)
+
+    suspend fun updateUnit(unit: Unit) = weatherDao.updateUnit(unit)
+
+    suspend fun deleteAllUnits() = weatherDao.deleteAllUnits()
+
+    suspend fun deleteUnit(unit: Unit) = weatherDao.deleteUnit(unit)
 
 }
