eb0050c15d89f8ec4d6a7102dfb3a343492e8aef
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-06 00:43:22 +0530

    Added Unit class to Database

--- app/src/main/java/com/example/weatherforecast/data/WeatherDatabase.kt
+++ app/src/main/java/com/example/weatherforecast/data/WeatherDatabase.kt
@@ -3,8 +3,9 @@ package com.example.weatherforecast.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.weatherforecast.model.Favourite
+import com.example.weatherforecast.model.Unit
 
-@Database(entities = [Favourite::class], version = 1, exportSchema = false)
+@Database(entities = [Favourite::class, Unit::class], version = 2, exportSchema = false)
 abstract class WeatherDatabase: RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
